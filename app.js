8f4eb31329d7005e2fb88548d5b8c54fa25137c5
MuhammadShaoib23 <shoaibgull1616@gmail.com>
2025-06-02 20:21:26 +0500

    4 question done

---
+++ app.js
@@ -0,0 +1,33 @@
+// Q1...
+// var userAge = prompt("Enter you age : ")
+// if(userAge >= 18 )
+//     alert("you are an adult")
+// else
+//     alert("You are a minor")
+// ============================================
+// Q2...
+// var num = prompt("Enter a number")
+// if(num >= 0)
+// console.log("Positive number");    
+// else
+//     console.log("Not Positive");
+// =============================================
+// Q3...
+// var num = prompt("Enter a number")
+// if(num % 2 === 0)
+//     alert("Even number")
+// else
+//     alert("Odd number")
+// ==============================================
+// Q4...
+// var username = prompt("Enter username ")
+// if(username === "admin")
+//     console.log("Welcome, admin!");
+// else
+//     console.log("Access denied");
+// ==========================================
+// Q5...
+
+
+    
+    
